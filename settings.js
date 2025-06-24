f7003103fcd47669d38bd5a395c3b3350ca62679
morganstixx <leendasmith21@gmail.com>
2025-06-24 18:46:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'morgan',
-  author: '‎',
+  author: '‎morgan',
   botName: "Morgan",
   botOwner: 'mrogan Stixx', // Your name
-  ownerNumber: '2348103767740', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2348177123102', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
